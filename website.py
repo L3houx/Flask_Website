0278305759c07721a0167115c4506727d44fbba9
Felix Lehoux <felix.lehoux@hotmail.com>
2018-05-01 16:21:24 -0400

    Updated section Experiences, projects. Removed useless files and code.

--- website.py
+++ website.py
@@ -4,7 +4,7 @@ from random import randint
 from forms import LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ThisIsATest'
+app.config['SECRET_KEY'] = 'My website'
 
 @app.route("/")
 def index():
@@ -13,7 +13,6 @@ def index():
 @app.route('/')
 def about():
     return render_template('index.html', title='Aboute Me')
-
 
 @app.route('/projects')
 def programmation():
@@ -26,46 +25,6 @@ def roadtrip():
 @app.route('/experiences')
 def passions():
     return render_template('experiences.html', title='Experiences')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/')
-    return render_template('login.html', title='Sign In', form=form)
-
-'''@app.route("/python")
-def hello():
-    return "Hello World aaaaaaaaaaa!"
-
-@app.route("/members")
-def members():
-    return "Members"
-
-@app.route("/members/<string:name>/")
-def getMember(name):
-    return name
-
-@app.route("/hello/<string:name>/")
-def helloName(name):
-    #return render_template(
-    #    'test.html',name=name)
-
-    quotes = [ "'If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is.' -- John Louis von Neumann ",
-               "'Computer science is no more about computers than astronomy is about telescopes' --  Edsger Dijkstra ",
-               "'To understand recursion you must first understand recursion..' -- Unknown",
-               "'You look at things that are and ask, why? I dream of things that never were and ask, why not?' -- Unknown",
-               "'Mathematics is the key and door to the sciences.' -- Galileo Galilei",
-               "'Not everyone will understand your journey. Thats fine. Its not their journey to make sense of. Its yours.' -- Unknown"  ]
-    randomNumber = randint(0,len(quotes)-1)
-    quote = quotes[randomNumber]
-
-    return render_template(
-        'test.html',**locals())
-'''
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
